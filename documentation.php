ea5bcf30d3f59d8b3fe3797c42d64dc17bcdb135
Arrow Sparrow <dev@aroceu.cloud>
2024-11-21 16:48:00 -0500

    Added documentation

---
+++ documentation.php
@@ -0,0 +1,38 @@
+<?php include('header.php') ?>
+
+<?php include('sidebar.php') ?>
+
+<h1>Documentation</h1>
+
+<p>I made this website in <strike color="#ffcee8">eight hours</strike> one evening (with polishing afterward), mostly because I could see with such clarity what I wanted it to look like. I thought I'd compiled a list like this before, but maybe not. I even checked the Wayback Machine, but all I could find were fics people had written for me that I'd saved locally to my computer.</p>
+
+<p>Having this website makes me feel a bit self-conscious, even though the reason I made this site is out of gratitude much more than anything else. I love showing off things other people have made me in the same way that I love showing off things that I made myself. I think creativity is awesome and it's even awesomer when it's made specifically for me. And over the years I've turned into a real collector of things that make me happy. This is one of such cases.</p>
+
+<p>Anyway, I'm grateful for always having so many creative friends, and running into other creatives along the way just to give to one another, for the fun of it. I've made networks of friends this way, even though I'd still be happy just collecting my little trinkets.</p>
+
+<h3>Design</h3>
+
+<p>This site's design came into full focus as soon as I had the vibe in my head: a cutesy pixely website. Well, <a href="https://aroceu.com/library">I had done that before</a>, but it <i>is</i> a timeless design, so I could do it again. And I did.</p>
+
+<p>Some design notes:</p>
+
+<ul><li>The only consistent image motif here is a <heart></heart> because that's really what this is all an extension of.</li> 
+	<li>Every image on this site has a heart (which isn't hard to figure out), and in tandem is also pink because of pink being my favorite color to customize with &mdash; the first color I chose to like.</li>
+	<li><big>Indie Flower</big> is the font used as an accent to keep the vibe of this site very cutesy and casual, much like my enthusiasm for the things archived here. </li>
+	<li>I continue to use Inconsolata as the main body text for consistency across similar pages.</li>
+	<li>The original idea for this site was to have it be a standard one column header above content, but then I realized that sidebar navigation would be much more useful considering the gallery aspect of it. I have made similar layouts before, but not with pixel art &mdash; and tbh, I think the pixel art really makes it pop.</li>
+	<li>I'd gotten inspired to make this in the first place by completely forgetting and then remembering this specific design of my old fic site (2017-2022, it wasn't even that long ago) and enjoying how I'd put the gallery together. Those have <a href="https://aroceu.com/fic/etc">their own home</a> now, but I've been commissioning a lot of art lately because it's my favorite thing to spend money on, and people have written me fics, so... here we are :D</li>
+</ul>
+
+<p>This site is currently not responsive/mobile-friendly. But maybe one day it will be.</p>
+
+<h3>Credits/Toolkit</h3>
+
+	<ul>
+	<li>The background is from <a href="https://goblin-heart.net/sadgrl/webmastery/downloads/tiledbgs">Sadgirl Online's tiled bgs.</a></li>
+	<li>The gallery uses <a href="https://fancyapps.com/fancybox/">FancyBox</a> for the zoom in gallery on click. You can use your keyboard arrow keys to navigate through as well.</li>
+	<li>Pixel art was found through <a href="https://pixel-soup.tumblr.com">pixel-soup</a> on Tumblr.</li>
+	<li>I actually coded this one from scratch rather than stealing my own code.</li>
+</ul>
+
+<?php include('footer.php') ?>
