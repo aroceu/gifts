59b17368987831b78079d76f72e609e66ffb195b
Arrow Sparrow <dev@aroceu.cloud>
2024-11-22 00:15:08 -0500

    Added some fic gift art

--- fanart/index.php
+++ fanart/index.php
@@ -18,11 +18,13 @@
 	<li><a class="fancybox" href="kenz-wwx-me.jpeg" data-fancybox-group="gallery" title="by Kenz"><img src="kenz-wwx-me.jpeg" alt=""/></a></li>
 	<li><a class="fancybox" href="jq-wwx-me.jpeg" data-fancybox-group="gallery" title="by JQ "><img src="jq-wwx-me.jpeg" alt=""/></a></li>
 	<li><a class="fancybox" href="yin-lwj.jpeg" data-fancybox-group="gallery" title="by yin (yin_yoru) - I'm not sure if this would count as being for me more than I prompted it but it's too sick for me not to show off here anyway"><img src="yin-lwj.jpeg" alt=""/></a></li>
+	<li><a class="fancybox" href="threeofwands-wangxian.jpeg" data-fancybox-group="gallery" title="by ThreeOfWands for MXTX Remix 2021"><img src="threeofwands-wangxian.jpeg" alt=""/></a></li>
 	<li><a class="fancybox" href="asher-pkmn.png" data-fancybox-group="gallery" title="by Asher"><img src="asher-pkmn.png" alt=""/></a></li>
 	<li><a class="fancybox" href="kelley-oisuga.jpg" data-fancybox-group="gallery" title="by Kelley"><img src="kelley-oisuga.jpg" alt=""/></a></li>
 	<li><a class="fancybox" href="jeanette-kagehina.png" data-fancybox-group="gallery" title="by Jeanette (paltita-sketch)"><img src="jeanette-kagehina.png" alt=""/></a></li>
 	<li><a class="fancybox" href="cathy-em.jpg" data-fancybox-group="gallery" title="by Cathy"><img src="cathy-em.jpg" alt=""/></a></li>
 	<li><a class="fancybox" href="katie-kunimi.png" data-fancybox-group="gallery" title=""><img src="katie-kunimi.png" alt=""/></a></li>
+	<li><a class="fancybox" href="feasted-kagehina.jpg" data-fancybox-group="gallery" title="by feasted as part of a personal exchange"><img src="feasted-kagehina.jpg" alt=""/></a></li>
 	<li><a class="fancybox" href="mishhe-kagehina.png" data-fancybox-group="gallery" title="by mishhe for interhigh"><img src="mishhe-kagehina.png" alt=""/></a></li>
 	<li><a class="fancybox" href="christina-roserade.PNG" data-fancybox-group="gallery" title=""><img src="christina-roserade.PNG" alt=""/></a></li>
 	<li><a class="fancybox" href="iforgot-bunny.png" data-fancybox-group="gallery" title=""><img src="iforgot-bunny.png" alt=""/></a></li>
