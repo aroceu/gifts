e2ddb2ba3cc4b424787ec8b1afa91f79cce7b30c
Arrow Sparrow <dev@aroceu.cloud>
2024-11-28 00:36:16 -0500

    Alt test on Splatoon

--- splatoon/index.php
+++ splatoon/index.php
@@ -8,39 +8,42 @@
 
 <p>My Splatoon octoling is how I see myself and how I'd like others to visualize my presence online, despite the fact that I have a physical form. I'm so attached to my octoling's design that I have commissioned just about the same amount of art for every other fandom combined as I have for art of my octoling. I've been playing since 2018, and I'm sure I'll commission more art with each new game.</p>
 
-<p>Some of this art has been gifted to me by friends. I was also on a competitive team, <big>Inkblot Dropouts</big>, for about a year, so some group art is here as well.</p>
+<p>Some of this art has been gifted to me by friends. I was also on a competitive team, <big>Inkblot Dropouts</big>, for about a year, so some group art is here. And my friend <a href="https://togekissies.tumblr.com">Rae</a> and I have been playing Splatoon together basically since we both got really invested in it in 2018, so we've commissioned some art of our octolings together as well.</p>
+
+<p>For alt text purposes: my octoling has tan skin, the feminine option ponytail, and bright pink ink. Her outfit in Splatoon 2 is a black bandana, hothouse hoodie, and Annaki Arachno boots, with the custom hydra splatling as her main weapon. In Splatoon 2, her outfit is the tentaclinger earring (sometimes visible), Moby knit, and the Annaki Tigers, with the vanilla tri-stringer (standard skin) as her main weapon.</p>
 
 <p>You can check out my octoling's vanity project at <a href="https://arrow.uwu.ai">arrow.uwu.ai</a>.</p>
 
 <ul id="gallery">
-	<li><a class="fancybox" href="s3-stupjam.png" data-fancybox-group="gallery" title="by stupjam"><img src="s3-stupjam.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-oveikeii.png" data-fancybox-group="gallery" title="by oveikeii"><img src="s3-oveikeii.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-annaswelle.png" data-fancybox-group="gallery" title="by AnnaSwelle"><img src="s3-annaswelle.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-kamumbo.png" data-fancybox-group="gallery" title="by Kamumbo"><img src="s3-kamumbo.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-armasdraws.jpg" data-fancybox-group="gallery" title="by ArmasDraws"><img src="s3-armasdraws.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-amipoo.jpg" data-fancybox-group="gallery" title="by Amipoo"><img src="s3-amipoo.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-1halfvanilla.jpg" data-fancybox-group="gallery" title="by 1halfvanilla"><img src="s3-1halfvanilla.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-evercaptor.png" data-fancybox-group="gallery" title="by Knife (evercaptor)"><img src="s3-evercaptor.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-mina.jpg" data-fancybox-group="gallery" title="by Mina"><img src="s3-mina.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s3-atoi_iota.png" data-fancybox-group="gallery" title="by atoi_iota"><img src="s3-atoi_iota.png" alt=""/></a></li>
+	<li><a class="fancybox" href="s3-stupjam.png" data-fancybox-group="gallery" title="by stupjam"><img src="s3-stupjam.png" alt="Digital art of my octoling in Splatoon 3 fighting alongside Kingdra from Pokemon. My tri-stringer is outputting pink ink while Kingdra is outputting dark blue ink, both colors blinding in the ink at our feet."/></a></li>
+	<li><a class="fancybox" href="s3-oveikeii.png" data-fancybox-group="gallery" title="by oveikeii"><img src="s3-oveikeii.png" alt="Digital art of two octolings in Splatoon 3: one is mine, and the other is Rae's. Rae's ink color is a slightly lighter pink than mine, and she is wearing the blue Annaki cuff and blue lo-tops and holding the vanilla Splattershot Pro. Our octolings are posed back to back like we're chill but having fun."/></a></li>
+	<li><a class="fancybox" href="s3-annaswelle.png" data-fancybox-group="gallery" title="by AnnaSwelle"><img src="s3-annaswelle.png" alt="Tableturf art of my octoling in Splatoon 3. The card name is Arrow and a 5-special grid shaped like a bow and arrow. The card itself is fresh and rare."/></a></li>
+	<li><a class="fancybox" href="s3-kamumbo.png" data-fancybox-group="gallery" title="by Kamumbo"><img src="s3-kamumbo.png" alt="Digital chibi art of my octoling in Splatoon 3 with her hand on her hip and looking confident."/></a></li>
+	<li><a class="fancybox" href="s3-armasdraws.jpg" data-fancybox-group="gallery" title="by ArmasDraws"><img src="s3-armasdraws.jpg" alt="Icon of my octoling in Splatoon 3 but in octoglasses and the Ananki flannel hoodie instead."/></a></li>
+	<li><a class="fancybox" href="s3-amipoo.jpg" data-fancybox-group="gallery" title="by Amipoo"><img src="s3-amipoo.jpg" alt="Digital art of my octoling in Splatoon 3 grinning at the camera, in a style brighter and bolder than others."/></a></li>
+	<li><a class="fancybox" href="s3-1halfvanilla.jpg" data-fancybox-group="gallery" title="by 1halfvanilla"><img src="s3-1halfvanilla.jpg" alt="Digital art of my octoling in Splatoon 3 holding a tri-stringer. The art style is similar to Splatoon's conceptual character art."/></a></li>
+	<li><a class="fancybox" href="s3-evercaptor.png" data-fancybox-group="gallery" title="by Knife (evercaptor)"><img src="s3-evercaptor.png" alt="Digital art of my octoling from Splatoon 3 in the ending pose of the Shot Caller emote, along with a splashtag with the title Reliable Narrator and gold badges for rainmaker, tri-stringer, and stinger from Salmon Run."/></a></li>
+	<li><a class="fancybox" href="s3-mina.jpg" data-fancybox-group="gallery" title="by Mina"><img src="s3-mina.jpg" alt="Character sheet of my octoling in headshot style in four poses: smiling, waving, looking confused, and winking with her tongue out."/></a></li>
+	<li><a class="fancybox" href="s3-atoi_iota.png" data-fancybox-group="gallery" title="by atoi_iota"><img src="s3-atoi_iota.png" alt="Icon of my octoling from Splatoon 3 holding a tri-stringer on a yellow background, in the same style as a previously commissioned Splatoon 2 icon."/></a></li>
 
 <!-- group art -->
 
-	<li><a class="fancybox" href="idx-heightchart.jpeg" data-fancybox-group="gallery" title="by Qul"><img src="idx-heightchart.jpeg" alt=""/></a></li>
-	<li><a class="fancybox" href="idx-callouts.png" data-fancybox-group="gallery" title="by Buzzy (oblivionoil)"><img src="idx-callouts.png" alt=""/></a></li>
-	<li><a class="fancybox" href="ar-sallyconigal.jpg" data-fancybox-group="gallery" title="by Sally Conigal"><img src="ar-sallyconigal.jpg" alt=""/></a></li>
+	<li><a class="fancybox" href="idx-heightchart.jpeg" data-fancybox-group="gallery" title="by Qul"><img src="idx-heightchart.jpeg" alt="Banner of sketches of five Splatoon characters but with varying heights. The shortest is holding a large inkbrush; the second-shortest is me with the bow; the next two are approximately the same height with a nautilus and a splatana stamper; and the last is much taller than the others with only the lower half of their body holding a luna blaster is visible."/></a></li>
+	<li><a class="fancybox" href="idx-callouts.png" data-fancybox-group="gallery" title="by Buzzy (oblivionoil)"><img src="idx-callouts.png" alt="Banner of simple doodles of five Splatoon characters (four octolings, including mine, and one inkling) as if they were finger puppets on a white background."/></a></li>
+	<li><a class="fancybox" href="ar-sallyconigal.jpg" data-fancybox-group="gallery" title="by Sally Conigal"><img src="ar-sallyconigal.jpg" alt="Digital art of my octoling from Splatoon 2 with Rae's, who is an octoling with her hair (tentacles) down, light blue ink, pilot goggles, a white-tshirt, and the dynamo roller. The octolings are posed next to each other and exuding confidence, with colored ink in swirls behind them on a black background."/></a></li>
 
 <!-- splatoon 2 -->
 
-	<li><a class="fancybox" href="s2-sallyconigal.jpg" data-fancybox-group="gallery" title="by SallyConigal"><img src="s2-sallyconigal.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-atoi_iota.jpg" data-fancybox-group="gallery" title="by atoi_iota"><img src="s2-atoi_iota.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-kaizoku-tako.png" data-fancybox-group="gallery" title="by Kaizuko_Tako"><img src="s2-kaizoku-tako.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-jupy314.png" data-fancybox-group="gallery" title="by Jupy314"><img src="s2-jupy314.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-iforgot.png" data-fancybox-group="gallery" title=""><img src="s2-iforgot.png" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-eladraws.jpg" data-fancybox-group="gallery" title="by ElaDraws"><img src="s2-eladraws.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-ash.jpg" data-fancybox-group="gallery" title="by Ash (gutrots)"><img src="s2-ash.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-anon.jpg" data-fancybox-group="gallery" title=""><img src="s2-anon.jpg" alt=""/></a></li>
-	<li><a class="fancybox" href="s2-inkayrider.png" data-fancybox-group="gallery" title="by inkayrider"><img src="s2-inkayrider.png" alt=""/></a></li>
+	<li><a class="fancybox" href="s2-sallyconigal.jpg" data-fancybox-group="gallery" title="by SallyConigal"><img src="s2-sallyconigal.jpg" alt="Icon of my octoling from Splatoon 2 holding the hydra splatling on a dark green background."/></a></li>
+	<li><a class="fancybox" href="s2-atoi_iota.jpg" data-fancybox-group="gallery" title="by atoi_iota"><img src="s2-atoi_iota.jpg" alt="Icon of my octoling from Splatoon 2 holding the hydra splatling on a light blue background."/></a></li>
+	<li><a class="fancybox" href="s2-kaizoku-tako.png" data-fancybox-group="gallery" title="by Kaizuko_Tako"><img src="s2-kaizoku-tako.png" alt="Two colored doodles of my octoling from Splatoon 2: one of her dabbing, and another of her crashing through a door with the text RAE HOLY FUCK in the style of the meme of Candace from Phineas and Ferb doing similarly."/></a></li>
+	<li><a class="fancybox" href="s2-jupy314.png" data-fancybox-group="gallery" title="by Jupy314"><img src="s2-jupy314.png" alt="Digital colored sketch of my octoling from Splatoon 2 holding a shiny amaura."/></a></li>
+	<li><a class="fancybox" href="s2-iforgot.png" data-fancybox-group="gallery" title=""><img src="s2-iforgot.png" alt="Digital drawing of my octoling from Splatoon 2 grinning while riding an aurorus (Pokémon.)"/></a></li>
+	<li><a class="fancybox" href="s2-eladraws.jpg" data-fancybox-group="gallery" title="by ElaDraws"><img src="s2-eladraws.jpg" alt="Icon of my octoling from Splatoon 2 on a blue X-styled background and holding the X rank pose."/></a></li>
+	<li><a class="fancybox" href="s2-ash.jpg" data-fancybox-group="gallery" title="by Ash (gutrots)"><img src="s2-ash.jpg" alt="Drawing of my octoling from Splatoon 2. She is winking with the custom hydra splatling next to her. 'Arrow' is written in pink along her side."/></a></li>
+	<li><a class="fancybox" href="s2-anon.jpg" data-fancybox-group="gallery" title=""><img src="s2-anon.jpg" alt="Sketch of my octoling from Splatoon 2. She is sitting with her chin in her hand, thinking in hearts the color of the sunset lesbian flag."/></a></li>
+	<li><a class="fancybox" href="s2-inkayrider.png" data-fancybox-group="gallery" title="by inkayrider"><img src="s2-inkayrider.png" alt="Icon of my octoling from Splatoon 2 on a light grey background. She is holding the custom hydra splatling and looking up in thought.
+"/></a></li>
 
 </ul>
 
