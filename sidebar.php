210347937426164abe754b557f675a46f1ebb548
Arrow Sparrow <dev@aroceu.cloud>
2024-12-01 15:18:48 -0500

    Fixed responsive issues

--- sidebar.php
+++ sidebar.php
@@ -6,18 +6,21 @@
 		<a href="https://aroceu.com/gifts"><img src="https://aroceu.com/gifts/img/pinkjewel.gif">
 		<br />for aroceu</div></a>
 
-	<div class="desktopnav">
-	<ul id="navigation">
-	<?php include('navigation.php') ?>
-	</ul>
-	</div>
-
 	<div class="mobilenav">
 		<div class="navigationbutton"><button style="" onclick="toggleNavigation()">Click for Navigation</button></div>
-		<ul id="navigation">
-		<?php include('navigation.php') ?>
-		<li><div class="close"><a onclick="toggleNavigation()">Close</a></div></li> <!-- make invisible on desktop -->
+	</div>
+
+	<ul id="navigation">
+	
+		<li><a href="https://aroceu.com/gifts/fanart"><img src="https://aroceu.com/gifts/img/redheart.gif"> Gifts (Fanart) <img src="https://aroceu.com/gifts/img/redheart.gif"></a></li>
+		<li><a href="https://aroceu.com/gifts/fic"><img src="https://aroceu.com/gifts/img/purpleheart.gif"> Gifts (Fic) <img src="https://aroceu.com/gifts/img/purpleheart.gif"></a></li>
+		<li><a href="https://aroceu.com/gifts/commissions"><img src="https://aroceu.com/gifts/img/pinkheart.gif"> Commissions (Fanart) <img src="https://aroceu.com/gifts/img/pinkheart.gif"></a></li>
+		<li><a href="https://aroceu.com/gifts/splatoon"><img src="https://aroceu.com/gifts/img/orangheart.gif"> Splatoon (Fanart) <img src="https://aroceu.com/gifts/img/orangheart.gif"></a></li>
+		
+	<div class="close"><li><a onclick="toggleNavigation()">Close</a></li></div> <!-- make invisible on desktop -->
 		</ul>
+
+		
 	</div>
 
 </div>
