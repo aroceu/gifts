03977c8d073f3efd206bcf3704f7384eb9357358
Arrow Sparrow <dev@aroceu.cloud>
2024-12-08 22:26:10 -0500

    Added sitemap in footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <div id="footer">
 
-	<p>&copy; 2009-<?php echo date("Y"); ?> aroceu <heart></heart> <a href="https://aroceu.com/gifts/documentation">documentation</a> <heart></heart> <a href="https://aroceu.com">contact me</a>  </p>
+	<p>&copy; 2009-<?php echo date("Y"); ?> aroceu <heart></heart> <a href="https://aroceu.com/gifts/documentation">documentation</a> <heart></heart> <a href="https://aroceu.com/atlas">sitemap</a> <heart></heart> <a href="https://aroceu.com">contact me</a>  </p>
 
 </div>
 
