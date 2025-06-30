015ed694155d2a6a767e2271b0823e60f0fbad2d
aroceu <dev@aroceu.cloud>
2025-06-30 00:14:04 -0400

    Rearranged some friend gifts

--- fic/friends.php
+++ fic/friends.php
@@ -10,6 +10,21 @@
 
 <ul id="ficlist">
 
+<!-- <li class="fic">
+	<fic><a href="https://archiveofourown.org/works/63563596">>Everybody Loves Fir</a> by BotheringBothering</fic>
+	<metadata><date>March 3, 2025</date></metadata>
+</li> -->
+
+<li class="fic">
+	<fic><a href="https://archiveofourown.org/works/61258723">Seen</a> by BotheringBothering</fic>
+	<metadata><date>December 11, 2024</date></metadata>
+</li>
+
+<li class="fic">
+	<fic><a href="https://archiveofourown.org/works/61024741">titles = conformity</a> by BotheringBothering</fic>
+	<metadata><date>December 2, 2024</date></metadata>
+</li>
+
 <li class="fic">
 <fic><a href="https://archiveofourown.org/works/42995406">from your head to your feet</a> by el_em_en_oh_pee</fic>
 <metadata><date>November 11, 2022</date></metadata>
@@ -80,22 +95,10 @@
 <metadata><date>February 11, 2011</date></metadata>
 </li>
 
-<li class="fic"><fic><offline>Private Lessons</offline> by xrosepetalsx</fic>
-	<metadata><date>2011</date></metadata></li>
-
 <li class="fic"><fic><offline>Bound</offline> by xrosepetalsx</fic>
 	<metadata><date>2011</date></metadata></li>
 
 <li class="fic"><fic><offline>Punishment</offline> by xrosepetalsx</fic>
-	<metadata><date>2011</date></metadata></li>
-
-<li class="fic"><fic><offline>Lust is Easy, Love is Hard, Like is most Important</offline> by dandanfung</fic>
-	<metadata><date>2011</date></metadata></li>
-
-<li class="fic"><fic><offline>the new york state of mind</offline> by Brixy</fic>
-	<metadata><date>2011</date></metadata></li>
-
-<li class="fic"><fic><offline>Break Each Piece of Me</offline> by Annie</fic>
 	<metadata><date>2011</date></metadata></li>
 
 <li class="fic"><fic><offline>The Art of Trying</offline> by Anne</fic>
@@ -104,52 +107,80 @@
 <li class="fic"><fic><offline>Chaos, turn off quiet</offline> by Ally</fic>
 	<metadata><date>2011</date></metadata></li>
 
+<li class="fic"><fic><offline>Lost to Be Found</offline> by Christie</fic>
+	<metadata><date>2010~2011</date></metadata></li>
+
+<li class="fic"><fic><offline>Ubiquitous</offline> by unactivist</fic>
+	<metadata><date>2010</date></metadata></li>
+
+<li class="fic"><fic><offline>Shutting down, Shutting up</offline> by Ally</fic>
+	<metadata><date>December 27, 2010</date></metadata></li>
+
+<li class="fic"><fic><a href="https://shihan-ai.livejournal.com/36586.html">Break Each Piece of Me</a> by Annie</fic>
+	<metadata><date>December 25, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>For Something You Thought You've Seen Before</offline> by theriverrunsred</fic>
+	<metadata><date>December 25, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>Love is like a Pi</offline> by heartl0ck</fic>
+	<metadata><date>December 2, 2010</date></metadata></li>
+
 <li class="fic">
 <fic><a href="https://gasume.livejournal.com/28153.html">Lost in Time</a> by genisaurion</fic>
 <metadata><date>November 11, 2010</date></metadata>
 </li>
 
-<li class="fic"><fic><offline>Maybe, He Loved Him Too.</offline> by akihikaru</fic>
-	<metadata><date>2010~2011</date></metadata></li>
+<li class="fic"><fic><offline>Virtual Pink Sparkling Hearts</offline> by kpoppin</fic>
+	<metadata><date>November 7, 2010</date></metadata></li>
 
-<li class="fic"><fic><offline>I like you. I like You.</offline> by akihikaru</fic>
-	<metadata><date>2010~2011</date></metadata></li>
+<li class="fic"><fic><offline>the new york state of mind</offline> by Brixy</fic>
+	<metadata><date>September 19, 2010</date></metadata></li>
 
-<li class="fic"><fic><offline>It's When the Sun Looks Like It's Never Going To Shine Again</offline> by akihikaru</fic>
-	<metadata><date>2010~2011</date></metadata></li>
+<li class="fic"><fic><offline>not a happy place</offline> by in_censors</fic>
+	<metadata><date>August 2, 2010</date></metadata></li>
 
-<li class="fic"><fic><offline>It's When Perfect Crumbles Away</offline> by akihikaru</fic>
-	<metadata><date>2010~2011</date></metadata></li>
+<li class="fic"><fic><offline>fake</offline> by yuefeng</fic>
+	<metadata><date>July 20, 2010</date></metadata></li>
 
-<li class="fic"><fic><offline>Lost to Be Found</offline> by Christie</fic>
-	<metadata><date>2010~2011</date></metadata></li>
-
-<li class="fic"><fic><offline>silly (love) story</offline> by prpldrkrse</fic>
-	<metadata><date>2010</date></metadata></li>
+<li class="fic"><fic><a href="https://un-gracious.livejournal.com/12547.html">disclaimed kingdoms</a> by unactivist</fic>
+	<metadata><date>July 1, 2010</date></metadata></li>
 
 <li class="fic"><fic><offline>I'm on a boat</offline> by Lune</fic>
-	<metadata><date>2010</date></metadata></li>
+	<metadata><date>June 20, 2010</date></metadata></li>
 
-<li class="fic"><fic><offline>It's Because I Love You</offline> by tsukixchan</fic>
-	<metadata><date>2010</date></metadata></li>
+<li class="fic"><fic><offline>It's Because I Miss You</offline> by tsukixchan</fic>
+	<metadata><date>June 13, 2010</date></metadata></li>
 
-<li class="fic"><fic><offline>Ubiquitous</offline> by unactivist</fic>
-	<metadata><date>2010</date></metadata></li>
-
-<li class="fic"><fic><offline>disclaimed kingdoms</offline> by unactivist</fic>
-	<metadata><date>2010</date></metadata></li>
-
-<li class="fic"><fic><offline>A Prelude of Us</offline> by unactivist</fic>
-	<metadata><date>2010</date></metadata></li>
-
-<li class="fic"><fic><offline>Love is like a Pi</offline> by heartl0ck</fic>
-	<metadata><date>2010</date></metadata></li>
-
-<li class="fic"><fic><offline>Virtual Pink Sparkling Hearts</offline> by kpoppin</fic>
-	<metadata><date>2010</date></metadata></li>
+<li class="fic"><fic><offline>Dreams come true.</offline> by catgirrl246</fic>
+	<metadata><date>June 13, 2010</date></metadata></li>
 
 <li class="fic"><fic><offline>notes & note passing</offline> by kpoppin</fic>
-	<metadata><date>2010</date></metadata></li>
+	<metadata><date>June 12, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>It's When the Sun Looks Like It's Never Going To Shine Again</offline> by akihikaru</fic>
+	<metadata><date>May 31, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>Private Lessons</offline> by xrosepetalsx</fic>
+	<metadata><date>May 13, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>silly (love) story</offline> by prpldrkrse</fic>
+	<metadata><date>May 2, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>Maybe, He Loved Him Too.</offline> by akihikaru</fic>
+	<metadata><date>April 24, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>I like you. I like You.</offline> by akihikaru</fic>
+	<metadata><date>April 21, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>A Prelude of Us</offline> by unactivist</fic>
+	<metadata><date>April 15, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>Lust is Easy, Love is Hard, Like is most Important</offline> by dandanfung</fic>
+	<metadata><date>April 9, 2010</date></metadata></li>
+
+<li class="fic"><fic><offline>It's When Perfect Crumbles Away</offline> by akihikaru</fic>
+	<metadata><date>March 28, 2010</date></metadata></li>
+
 
 </ul>
 
